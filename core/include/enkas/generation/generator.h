1b1536fc21c82bb5c960c59160f2894636fd9f01
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-05 14:55:14 +0200

    refactor: update include, namespaces and add default destructor

--- core/include/enkas/generation/generator.h
+++ core/include/enkas/generation/generator.h
@@ -2,10 +2,17 @@
 
 #include <vector>
 
-#include "utils.h"
+#include <enkas/data/particle.h>
+
+namespace enkas {
+namespace generation {
 
 class Generator
 {
 public:
-    virtual utils::InitialSystem createSystem() = 0;
+     virtual ~Generator() = default;
+    [[nodiscard]] virtual InitialSystem createSystem() = 0;
 };
+
+} // namespace generation
+} // namespace enkas
