abb3bebb33a45b34068149e66eba69f1aa49a24b
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-05 15:17:54 +0200

    refactor: move BaseParticle out of utils.h

---
+++ core/include/enkas/data/particle.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+#include <enkas/math/vector3d.h>
+
+namespace enkas {
+namespace data {
+
+struct BaseParticle
+{
+    Vector3D pos{};
+    Vector3D vel{};
+    double mass = 0.0;
+};
+
+using InitialSystem = std::vector<BaseParticle>;
+
+} // namespace data
+} // namespace enkas
