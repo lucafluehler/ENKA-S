43995f5fc2b3e97e3596b94ddddf101b37821ec9
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-07 12:51:10 +0200

    refactor: new system type

--- core/src/generation/generators/uniform_sphere_generator.cpp
+++ core/src/generation/generators/uniform_sphere_generator.cpp
@@ -1,52 +1,51 @@
 #include <random>
 #include <vector>
 
-#include <enkas/data/initial_system.h>
+#include <enkas/data/system.h>
 #include <enkas/generation/generators/uniform_sphere_generator.h>
 #include <enkas/physics/physics_helpers.h>
 
 namespace enkas::generation {
 
 UniformSphereGenerator::UniformSphereGenerator(const UniformSphereSettings& settings, unsigned int seed)
-    : settings(settings)
-    , seed(seed)
+    : settings_(settings)
+    , seed_(seed)
 {}
 
 data::InitialSystem UniformSphereGenerator::createSystem()
 {
-    data::InitialSystem initial_system;
-    initial_system.reserve(settings.N);
+    data::System system;
+    const int particle_count = settings_.particle_count;
+
+    system.positions.reserve(particle_count);
+    system.velocities.reserve(particle_count);
+    system.masses.reserve(particle_count);
 
     std::mt19937 gen(seed);
-    std::uniform_real_distribution<double> pos_dist(-settings.radius, settings.radius);
+    std::uniform_real_distribution<double> pos_dist(-settings_.sphere_radius, settings_.sphere_radius);
     std::uniform_real_distribution<double> vel_dist(0.0, 1.0);
 
-    for (size_t i = 0; i < settings.N; i++) {
-        data::BaseParticle particle;
+    const double particle_mass = m_settings.total_mass / particle_count;
 
-        // POSITION
+    for (size_t i = 0; i < particle_count; i++) {
         math::Vector3D position;
 
         // Use a rejection technique to carve out a homogeneous sphere from a homogeneous cube.
         do {
-            position = math::Vector3D(pos_dist(gen), pos_dist(gen), pos_dist(gen));
-        } while (position.norm() > settings.radius);
+            position = {pos_dist(gen), pos_dist(gen), pos_dist(gen)};
+        } while (position.norm() > settings_.sphere_radius);
 
-        particle.pos = position;
+        math::Vector3D velocity = {vel_dist(gen), vel_dist(gen), vel_dist(gen)};
+        velocity.set_norm(settings_.initial_velocity);
 
-        // VELOCITY
-        particle.vel = math::Vector3D(vel_dist(gen), vel_dist(gen), vel_dist(gen));
-        particle.vel.set_norm(settings.vel);
-
-        // MASS
-        particle.mass = settings.total_mass/settings.N;
-
-        initial_system.push_back(particle);
+        system.positions.push_back(position);
+        system.velocities.push_back(velocity);
+        system.masses.push_back(particle_mass);
     }
 
-    physics::centerParticles(initial_system);
+    physics::centerSystem(system);
 
-    return initial_system;
+    return system;
 }
 
 } // namespace enkas::generation
