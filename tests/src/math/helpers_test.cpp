14d7428df52163d6028b742053e5f567d19304c3
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-09 12:21:46 +0200

    tests: add test to check uniformity

--- tests/src/math/helpers_test.cpp
+++ tests/src/math/helpers_test.cpp
@@ -12,3 +12,21 @@ TEST(RandOnSphereTests, NormIsCorrect) {
 
     EXPECT_NEAR(actual_norm, expected_norm, tolerance);
 }
+
+TEST(RandOnSphereTests, DistributionIsUniform) {
+    const unsigned int seed = 12345;
+    std::mt19937 generator(seed);
+    const unsigned int sample_count = 100000;
+
+    enkas::math::Vector3D mean_vector;
+
+    for (unsigned int i = 0; i < sample_count; ++i) {
+        auto vec = enkas::math::getRandOnSphere(generator);
+        mean_vector += vec*(1.0/sample_count);
+    }
+
+    // The mean vector magnitude should be close to zero for a 
+    // uniform distribution on the sphere
+    const double tolerance = 1e-2;
+    EXPECT_NEAR(mean_vector.norm(), 0.0, tolerance);
+}
