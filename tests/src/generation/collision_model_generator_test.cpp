5f1bc9463bab70949a30f4b0e3e1998207e0bbb0
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-10 13:49:04 +0200

    tests: add collision model tests

---
+++ tests/src/generation/collision_model_generator_test.cpp
@@ -0,0 +1,67 @@
+#include <enkas/generation/generators/collision_model_generator.h>
+#include <enkas/physics/helpers.h>
+#include <gtest/gtest.h>
+
+class CollisionModelGeneratorTest : public ::testing::Test {
+protected:
+    enkas::generation::CollisionModelSettings settings{};
+
+    void SetUp() override {
+        settings.impact_parameter = 1.0;
+        settings.relative_velocity = 1.0;
+
+        settings.particle_count_1 = 100;
+        settings.sphere_radius_1 = 10.0;
+        settings.total_mass_1 = 1.0;
+
+        settings.particle_count_2 = 100;
+        settings.sphere_radius_2 = 10.0;
+        settings.total_mass_2 = 1.0;
+    }
+};
+
+TEST_F(CollisionModelGeneratorTest, CreateSystem) {
+    enkas::generation::CollisionModelGenerator generator(settings, 42);
+    enkas::data::System system = generator.createSystem();
+
+    EXPECT_EQ(system.positions.size(), settings.particle_count_1 + settings.particle_count_2);
+    EXPECT_EQ(system.velocities.size(), settings.particle_count_1 + settings.particle_count_2);
+    EXPECT_EQ(system.masses.size(), settings.particle_count_1 + settings.particle_count_2);
+
+    // Test position and velocity distributions
+    // ...
+
+    const double total_mass = std::accumulate(system.masses.begin(), system.masses.end(), 0.0);
+    EXPECT_NEAR(total_mass, settings.total_mass_1 + settings.total_mass_2, 1e-6);
+}
+
+TEST_F(CollisionModelGeneratorTest, SystemCentered) {
+    enkas::generation::CollisionModelGenerator generator(settings, 42);
+    enkas::data::System system = generator.createSystem();
+
+    auto com = enkas::physics::getCenterOfMass(system);
+    const double tolerance = 1e-9;
+    EXPECT_NEAR(com.position.x, 0.0, tolerance);
+    EXPECT_NEAR(com.position.y, 0.0, tolerance);
+    EXPECT_NEAR(com.position.z, 0.0, tolerance);
+
+    EXPECT_NEAR(com.velocity.x, 0.0, tolerance);
+    EXPECT_NEAR(com.velocity.y, 0.0, tolerance);
+    EXPECT_NEAR(com.velocity.z, 0.0, tolerance);
+}
+
+TEST_F(CollisionModelGeneratorTest, Reproducibility) {
+    enkas::generation::CollisionModelGenerator generator1(settings, 42);
+    enkas::data::System system1 = generator1.createSystem();
+
+    enkas::generation::CollisionModelGenerator generator2(settings, 42);
+    enkas::data::System system2 = generator2.createSystem();
+
+    ASSERT_EQ(system1.positions.size(), system2.positions.size());
+
+    for (size_t i = 0; i < system1.positions.size(); ++i) {
+        EXPECT_EQ(system1.positions[i], system2.positions[i]);
+        EXPECT_EQ(system1.velocities[i], system2.velocities[i]);
+        EXPECT_DOUBLE_EQ(system1.masses[i], system2.masses[i]);
+    }
+}
