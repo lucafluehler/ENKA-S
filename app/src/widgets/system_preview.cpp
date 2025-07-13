9e84aa95b95499c41709e5a470bc7b621069250e
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-13 15:18:43 +0200

    refactor: system preview to v2 arch

---
+++ app/src/widgets/system_preview.cpp
@@ -0,0 +1,96 @@
+#include "system_preview.h"
+
+#include <enkas/generation/generation_factory.h>
+#include <enkas/generation/generation_method.h>
+#include <enkas/generation/generator.h>
+
+#include <fstream>
+
+#include "../core/settings.h"
+#include "enkas/generation/settings/collision_model_settings.h"
+
+SystemPreview::SystemPreview(QWidget* parent) : ParticleRenderer(parent) {
+    // Set default Render Settings
+    RenderSettings render_settings;
+    render_settings.coloring_method = ColoringMethod::WHITE_FOG;
+    render_settings.show_center_of_mass = false;
+    render_settings.show_center_of_screen = false;
+
+    redraw(render_settings);
+}
+
+void SystemPreview::initializeProcedural(enkas::generation::Method method) {
+    setMethodSettings();
+
+    // Generate system
+    auto settings = Settings();
+    settings.setSetting(
+        "GenerationMethod",
+        Setting(Setting::Group::Generation, Setting::Type::GenerationMethod, method));
+    auto config = settings.getGenerationConfig();
+    auto system = enkas::generation::Factory::create(config)->createSystem();
+
+    // Feed system to particle renderer
+    updateData(std::make_shared<enkas::data::System>(system));
+}
+
+void SystemPreview::initializeFromFile(const QString& initial_system_path) {
+    setMethodSettings();
+
+    // Load system from file
+    std::ifstream stream(initial_system_path.toStdString());
+    if (!stream.is_open()) {
+        throw std::runtime_error(
+            std::format("Failed to open system file: {}", initial_system_path.toStdString()));
+    }
+    auto system = enkas::generation::Factory::create(stream)->createSystem();
+
+    // Feed system to particle renderer
+    updateData(std::make_shared<enkas::data::System>(system));
+}
+
+void SystemPreview::initializeHomeScreen() {
+    setHomeScreenSettings();
+
+    // Generate system
+    enkas::generation::CollisionModelSettings settings;
+    settings.particle_count_1 = 1200;
+    settings.sphere_radius_1 = 0.6;
+    settings.particle_count_2 = 1200;
+    settings.sphere_radius_2 = 0.6;
+    auto config = enkas::generation::Config();
+    config.seed = 42;
+    config.specific_settings = settings;
+    auto system = enkas::generation::Factory::create(config)->createSystem();
+
+    // Feed system to particle renderer
+    updateData(std::make_shared<enkas::data::System>(system));
+}
+
+void SystemPreview::setMethodSettings() {
+    // Render Settings
+    RenderSettings render_settings;
+    render_settings.animation_style = AnimationStyle::TUTTI;
+    render_settings.animation_speed = 1.4;
+    render_settings.coloring_method = ColoringMethod::WHITE_FOG;
+    render_settings.show_center_of_mass = false;
+    render_settings.show_center_of_screen = false;
+    render_settings.fov = 50;
+    render_settings.particle_size_param = 1.2;
+
+    redraw(render_settings);
+}
+
+void SystemPreview::setHomeScreenSettings() {
+    // Render Settings
+    RenderSettings render_settings;
+    render_settings.animation_style = AnimationStyle::UP;
+    render_settings.animation_speed = 1;
+    render_settings.coloring_method = ColoringMethod::WHITE_FOG;
+    render_settings.show_center_of_mass = false;
+    render_settings.show_center_of_screen = false;
+    render_settings.fov = 60;
+    render_settings.particle_size_param = 0.4;
+
+    redraw(render_settings);
+}
---
+++ app/src/widgets/system_preview.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <enkas/generation/generation_method.h>
+
+#include <QObject>
+#include <QOpenGLFunctions>
+#include <QOpenGLWidget>
+
+#include "../rendering/particle_renderer.h"
+
+class SystemPreview : public ParticleRenderer {
+    Q_OBJECT
+
+public:
+    explicit SystemPreview(QWidget* parent = nullptr);
+
+    /**
+     * @brief Initializes the preview with a procedurally generated system based on the given
+     * method.
+     * @param method The generation method to use.
+     */
+    void initializeProcedural(enkas::generation::Method method);
+
+    /**
+     * @brief Initializes the preview with a system loaded from a file.
+     * @param initial_system_path The path to the system file.
+     */
+    void initializeFromFile(const QString& initial_system_path);
+
+    /**
+     * @brief Initializes the preview for the home screen with a default system.
+     */
+    void initializeHomeScreen();
+
+private:
+    /**
+     * @brief Sets the render settings for the procedural and file-based systems.
+     */
+    void setMethodSettings();
+
+    /**
+     * @brief Sets the render settings for the home screen system.
+     */
+    void setHomeScreenSettings();
+};
