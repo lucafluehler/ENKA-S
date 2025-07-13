9a15c7dbfd205c6983829773feb9c9c7a04eb885
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-13 21:57:45 +0200

    fix: make methods public

--- app/src/core/file_check_logic.h
+++ app/src/core/file_check_logic.h
@@ -3,6 +3,7 @@
 #include <filesystem>
 
 class FileCheckLogic {
+public:
     static bool checkSettingsFile(const std::filesystem::path& file_path);
     static bool checkSystemFile(const std::filesystem::path& file_path);
     static bool checkDiagnosticsFile(const std::filesystem::path& file_path);
