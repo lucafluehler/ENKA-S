173b1c965df25d34960a53b1336bae09603817ef
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-15 23:56:17 +0200

    feat: interface for settings widgets

---
+++ app/src/views/new_simulation_tab/settings_widgets/settings_widget.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../../../core/settings.h"
+
+class SettingsWidget {
+public:
+    /**
+     * @brief Retrieves the current settings from the panel.
+     * @return A Settings object containing the current settings.
+     */
+    virtual Settings getSettings() const;
+
+    /**
+     * @brief Sets the settings of the panel.
+     * @param settings The Settings object to set.
+     */
+    virtual void setSettings(const Settings& settings);
+
+    /**
+     * @brief Sets the default settings for the panel. These are applied when resetting the panel.
+     * @param settings The Settings object to set as default.
+     */
+    virtual void setDefaultSettings(const Settings& settings);
+
+    /**
+     * @brief Resets the settings of the panel to the default values.
+     */
+    virtual void resetSettings();
+};
