5cc563a7b440ad2a03e8e902f4dc8f0ec13e3cde
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-16 15:09:31 +0200

    feat: add csv headers

--- app/src/core/file_names.h
+++ app/src/core/file_names.h
@@ -1,13 +1,33 @@
 #pragma once
 
-#include <string_view>
+#include <string>
+#include <vector>
 
 namespace file_names {
-inline constexpr std::string_view settings = "settings.csv";
-inline constexpr std::string_view system = "system.csv";
-inline constexpr std::string_view diagnostics = "diagnostics.csv";
+inline constexpr char settings[] = "settings.csv";
+inline constexpr char system[] = "system.csv";
+inline constexpr char diagnostics[] = "diagnostics.csv";
 }  // namespace file_names
 
 namespace folder_names {
-inline constexpr std::string_view output = "output";
+inline constexpr char output[] = "output";
 }  // namespace folder_names
+
+namespace csv_headers {
+inline const std::vector<std::string> system = {
+    "time", "pos_x", "pos_y", "pos_z", "vel_x", "vel_y", "vel_z", "mass"};
+
+inline const std::vector<std::string> diagnostics = {"time",
+                                                     "e_kin",
+                                                     "e_pot",
+                                                     "L_tot",
+                                                     "com_pos_x",
+                                                     "com_pos_y",
+                                                     "com_pos_z",
+                                                     "com_vel_x",
+                                                     "com_vel_y",
+                                                     "com_vel_z",
+                                                     "r_vir",
+                                                     "ms_vel",
+                                                     "t_cr"};
+}  // namespace csv_headers
