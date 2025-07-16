2f826efa5253a5ba07348e9bd6281b4a7f0f7e98
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-16 20:43:10 +0200

    feat: replace data ptr with snapshots

---
+++ app/src/core/snapshot.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <enkas/data/diagnostics.h>
+#include <enkas/data/system.h>
+
+#include <memory>
+
+template <typename T>
+struct Timed {
+    double time;
+    T data;
+
+    Timed(double t, T d) : time(t), data(std::move(d)) {}
+};
+
+using DiagnosticsSnapshotPtr = std::shared_ptr<Timed<enkas::data::Diagnostics>>;
+using SystemSnapshotPtr = std::shared_ptr<Timed<enkas::data::System>>;
