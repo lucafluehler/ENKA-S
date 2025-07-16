a1349105f06664f5d7bd34a7b105eca99b1c9bc6
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-16 13:24:12 +0200

    docs: add doxygen comments

--- app/src/core/data_storage_logic.h
+++ app/src/core/data_storage_logic.h
@@ -8,10 +8,29 @@
 
 class DataStorageLogic {
 public:
+    /**
+     * @brief Saves the settings to a JSON file in the specified directory.
+     * @param dir_path The directory path where the settings file will be saved.
+     * @param settings The settings to save.
+     */
     static void saveSettings(const std::filesystem::path& dir_path, const Settings& settings);
+
+    /**
+     * @brief Saves the system data to a CSV file in the specified directory.
+     * @param dir_path The directory path where the system data file will be saved.
+     * @param time The timestamp of the system data.
+     * @param system The system data to save.
+     */
     static void saveSystemData(const std::filesystem::path& dir_path,
                                double time,
                                const enkas::data::System& system);
+
+    /**
+     * @brief Saves the diagnostics data to a CSV file in the specified directory.
+     * @param dir_path The directory path where the diagnostics data file will be saved.
+     * @param time The timestamp of the diagnostics data.
+     * @param diagnostics The diagnostics data to save.
+     */
     static void saveDiagnosticsData(const std::filesystem::path& dir_path,
                                     double time,
                                     const enkas::data::Diagnostics& diagnostics);
