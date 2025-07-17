f0d1e043c2a071fb04250f29535c4a193d62aa5b
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-17 11:39:07 +0200

    feat: add alias for non pointers

--- app/src/core/snapshot.h
+++ app/src/core/snapshot.h
@@ -14,6 +14,9 @@ struct Timed {
     Timed(double t, T d) : time(t), data(std::move(d)) {}
 };
 
+using SystemSnapshot = const Timed<enkas::data::System>;
+using DiagnosticsSnapshot = const Timed<enkas::data::Diagnostics>;
+
 using SystemSnapshotPtr = std::shared_ptr<const Timed<enkas::data::System>>;
 using DiagnosticsSnapshotPtr = std::shared_ptr<const Timed<enkas::data::Diagnostics>>;
 
