9cd92592240944b2e4c661d09fd4cf327c1fb875
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-17 21:43:54 +0200

    fix: correct ui names

--- app/src/views/main_window/main_window.cpp
+++ app/src/views/main_window/main_window.cpp
@@ -12,15 +12,15 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui_(new Ui::MainW
     ui_->setupUi(this);
 
     // Initialize the load simulation tab
-    load_simulation_presenter_ = new LoadSimulationPresenter(ui_->loadSimulationTab);
-    connect(ui_->loadSimulationTab,
+    load_simulation_presenter_ = new LoadSimulationPresenter(ui_->tabLoadSimulation);
+    connect(ui_->tabLoadSimulation,
             &LoadSimulationTab::requestFilesCheck,
             load_simulation_presenter_,
             &LoadSimulationPresenter::checkFiles);
 
     connect(ui_->tabWidget, &QTabWidget::currentChanged, this, [this](int index) {
         QWidget* current = ui_->tabWidget->widget(index);
-        if (current == ui_->loadSimulationTab) {
+        if (current == ui_->tabLoadSimulation) {
             load_simulation_presenter_->active();
         } else {
             load_simulation_presenter_->inactive();
@@ -28,24 +28,24 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui_(new Ui::MainW
     });
 
     // Initialize the new simulation tab
-    new_simulation_presenter_ = new NewSimulationPresenter(ui_->newSimulationTab);
-    connect(ui_->newSimulationTab,
+    new_simulation_presenter_ = new NewSimulationPresenter(ui_->tabNewSimulation);
+    connect(ui_->tabNewSimulation,
             &NewSimulationTab::checkInitialSystemFile,
             new_simulation_presenter_,
             &NewSimulationPresenter::checkInitialSystemFile);
-    connect(ui_->newSimulationTab,
+    connect(ui_->tabNewSimulation,
             &NewSimulationTab::checkSettingsFile,
             new_simulation_presenter_,
             &NewSimulationPresenter::checkSettingsFile);
-    connect(ui_->newSimulationTab,
+    connect(ui_->tabNewSimulation,
             &NewSimulationTab::requestSimulationStart,
             new_simulation_presenter_,
             &NewSimulationPresenter::startSimulation);
-    connect(ui_->newSimulationTab,
+    connect(ui_->tabNewSimulation,
             &NewSimulationTab::requestSimulationAbort,
             new_simulation_presenter_,
             &NewSimulationPresenter::abortSimulation);
-    connect(ui_->newSimulationTab->btnOpenLiveWindow,
+    connect(ui_->tabNewSimulation->btnOpenLiveWindow,
             &QPushButton::clicked,
             new_simulation_presenter_,
             &NewSimulationPresenter::openSimulationWindow);
