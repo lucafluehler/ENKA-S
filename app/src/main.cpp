7cc154108715b3ee1d1c4e0cdf9dec2db29a1ad0
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-18 15:57:17 +0200

    feat: workaround to avoid darkmode

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,6 +4,7 @@
 #include "views/main_window/main_window.h"
 
 int main(int argc, char *argv[]) {
+    qputenv("QT_QPA_PLATFORM", QByteArray("windows:darkmode=0"));
     QApplication app(argc, argv);
 
     MainWindow main_window;
