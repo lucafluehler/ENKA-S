fd5b7f593e38b28edc66833101d7fa40414cb83c
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-19 11:44:27 +0200

    feat: add limits helper

--- app/src/views/new_simulation_tab/settings_widgets/setting_descriptor.h
+++ app/src/views/new_simulation_tab/settings_widgets/setting_descriptor.h
@@ -5,6 +5,16 @@
 
 #include "core/settings/setting_key.h"
 
+namespace limits {
+constexpr double zero = 0.0;
+constexpr double smallest_greater_than_zero = 1e-15;
+constexpr double double_max = 1e10;
+constexpr int particle_count_min = 1;
+constexpr int particle_count_max = 1'000'000;
+constexpr int random_min = 0;
+constexpr int random_max = 1'000'000'000;
+}  // namespace limits
+
 struct SettingDescriptor {
     SettingKey key;
     QString label;
