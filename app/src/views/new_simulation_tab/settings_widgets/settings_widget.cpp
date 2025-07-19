2475ce872ac496a4f7f4f696db69453e1431dcf5
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-19 12:10:53 +0200

    fix: add guard and use simpler delete process

--- app/src/views/new_simulation_tab/settings_widgets/settings_widget.cpp
+++ app/src/views/new_simulation_tab/settings_widgets/settings_widget.cpp
@@ -133,10 +133,7 @@ void SettingsWidget::setSettings(const Settings& settings) {
 }
 
 void SettingsWidget::clearLayout(QFormLayout* layout) {
-    while (QLayoutItem* item = layout->takeAt(0)) {
-        if (QWidget* widget = item->widget()) {
-            widget->deleteLater();
-        }
-        delete item;
+    while (layout->rowCount() > 0) {
+        layout->removeRow(0);
     }
 }
