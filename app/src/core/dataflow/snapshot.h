4f55d952998fb91355a199a9e82cd5f31d0b6942
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-25 16:33:12 +0200

    refactor: rename to snapshot and add default ctor and reset function

--- app/src/core/dataflow/snapshot.h
+++ app/src/core/dataflow/snapshot.h
@@ -6,18 +6,24 @@
 #include <memory>
 
 template <typename T>
-struct Timed {
+struct Snapshot {
     std::shared_ptr<T> data;
     double time;
 
-    Timed(std::shared_ptr<T> d, double t = 0.0) : data(std::move(d)), time(t) {}
-    Timed(T d, double t = 0.0) : data(std::make_shared<T>(std::move(d))), time(t) {}
+    Snapshot() : data(nullptr), time(0.0) {}
+    Snapshot(std::shared_ptr<T> d, double t = 0.0) : data(std::move(d)), time(t) {}
+    Snapshot(T d, double t = 0.0) : data(std::make_shared<T>(std::move(d))), time(t) {}
+
+    void reset() {
+        data.reset();
+        time = 0.0;
+    }
 };
 
-using SystemSnapshot = const Timed<enkas::data::System>;
-using DiagnosticsSnapshot = const Timed<enkas::data::Diagnostics>;
+using SystemSnapshot = const Snapshot<enkas::data::System>;
+using DiagnosticsSnapshot = const Snapshot<enkas::data::Diagnostics>;
 
-using DiagnosticsSeries = std::vector<Timed<enkas::data::Diagnostics>>;
+using DiagnosticsSeries = std::vector<Snapshot<enkas::data::Diagnostics>>;
 
-using SystemSnapshotPtr = std::shared_ptr<const Timed<enkas::data::System>>;
-using DiagnosticsSnapshotPtr = std::shared_ptr<const Timed<enkas::data::Diagnostics>>;
+using SystemSnapshotPtr = std::shared_ptr<const Snapshot<enkas::data::System>>;
+using DiagnosticsSnapshotPtr = std::shared_ptr<const Snapshot<enkas::data::Diagnostics>>;
