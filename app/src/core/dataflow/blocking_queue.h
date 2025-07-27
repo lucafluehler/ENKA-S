d5c190074e5ad604328e9468307ee8c4d1897bec
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-27 22:55:56 +0200

    fix: remove lock from size method

--- app/src/core/dataflow/blocking_queue.h
+++ app/src/core/dataflow/blocking_queue.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <condition_variable>
 #include <deque>
 #include <mutex>
@@ -7,7 +8,7 @@
 template <typename T>
 class BlockingQueue {
 public:
-    explicit BlockingQueue(size_t max_size) : max_size_(max_size) {}
+    explicit BlockingQueue(size_t max_size) : max_size_(max_size), current_size_(0) {}
 
     /**
      * @brief Pushes an item into the queue, blocking until space is available.
@@ -15,8 +16,11 @@ public:
      */
     void pushBlocking(T item) {
         std::unique_lock lock(mutex_);
-        cond_full_.wait(lock, [&]() { return buffer_.size() < max_size_; });
+        cond_full_.wait(
+            lock, [&]() { return current_size_.load(std::memory_order_relaxed) < max_size_; });
         buffer_.push_back(std::move(item));
+        current_size_.fetch_add(1, std::memory_order_relaxed);
+        lock.unlock();
         cond_empty_.notify_one();
     }
 
@@ -29,6 +33,8 @@ public:
         cond_empty_.wait(lock, [&]() { return !buffer_.empty(); });
         T item = std::move(buffer_.front());
         buffer_.pop_front();
+        current_size_.fetch_sub(1, std::memory_order_relaxed);
+        lock.unlock();
         cond_full_.notify_one();
         return item;
     }
@@ -37,10 +43,7 @@ public:
      * @brief Returns the number of items currently in the queue.
      * @return The current size of the queue.
      */
-    [[nodiscard]] size_t size() const {
-        std::scoped_lock lock(mutex_);
-        return buffer_.size();
-    }
+    [[nodiscard]] size_t size() const { return current_size_.load(std::memory_order_relaxed); }
 
 private:
     mutable std::mutex mutex_;
@@ -48,4 +51,5 @@ private:
     std::condition_variable cond_empty_;
     std::deque<T> buffer_;
     size_t max_size_;
+    std::atomic<size_t> current_size_;
 };
