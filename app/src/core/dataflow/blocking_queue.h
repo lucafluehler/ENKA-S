133f68a7f705391e90d8a3f85efb05076550e9df
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-07-29 10:51:30 +0200

    refactor: add single sourth of truth for buffer size and make thread safe

--- app/src/core/dataflow/blocking_queue.h
+++ app/src/core/dataflow/blocking_queue.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <atomic>
 #include <condition_variable>
 #include <deque>
 #include <mutex>
@@ -8,7 +7,7 @@
 template <typename T>
 class BlockingQueue {
 public:
-    explicit BlockingQueue(size_t max_size) : max_size_(max_size), current_size_(0) {}
+    explicit BlockingQueue(size_t max_size) : max_size_(max_size) {}
 
     /**
      * @brief Pushes an item into the queue, blocking until space is available.
@@ -16,10 +15,11 @@ public:
      */
     void pushBlocking(T item) {
         std::unique_lock lock(mutex_);
-        cond_full_.wait(
-            lock, [&]() { return current_size_.load(std::memory_order_relaxed) < max_size_; });
+        // Wait until the buffer is not full
+        cond_full_.wait(lock, [&]() { return buffer_.size() < max_size_; });
+
         buffer_.push_back(std::move(item));
-        current_size_.fetch_add(1, std::memory_order_relaxed);
+
         lock.unlock();
         cond_empty_.notify_one();
     }
@@ -28,28 +28,35 @@ public:
      * @brief Pops an item from the queue, blocking until an item is available.
      * @return The item popped from the queue.
      */
-    T popBlocking() {
+    [[nodiscard]] T popBlocking() {
         std::unique_lock lock(mutex_);
+        // Wait until the buffer is not empty
         cond_empty_.wait(lock, [&]() { return !buffer_.empty(); });
+
         T item = std::move(buffer_.front());
         buffer_.pop_front();
-        current_size_.fetch_sub(1, std::memory_order_relaxed);
+
+        // Manual unlock before notify
         lock.unlock();
         cond_full_.notify_one();
+
         return item;
     }
 
     /**
      * @brief Returns the number of items currently in the queue.
      * @return The current size of the queue.
+     * @note This locks the mutex, so it is a thread-safe but potentially blocking call.
      */
-    [[nodiscard]] size_t size() const { return current_size_.load(std::memory_order_relaxed); }
+    [[nodiscard]] size_t size() const {
+        std::scoped_lock lock(mutex_);  // scoped_lock is great for simple acquisitions
+        return buffer_.size();
+    }
 
 private:
     mutable std::mutex mutex_;
     std::condition_variable cond_full_;
     std::condition_variable cond_empty_;
     std::deque<T> buffer_;
-    size_t max_size_;
-    std::atomic<size_t> current_size_;
+    const size_t max_size_;
 };
