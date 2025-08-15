0146474a10f1a1ca9de892b39f9ce598a5db9178
Luca Flühler <131798379+lucafluehler@users.noreply.github.com>
2025-08-15 17:13:45 +0200

    refactor: pass child presenters to manage active / inactive states

--- app/src/presenters/main_window_presenter.cpp
+++ app/src/presenters/main_window_presenter.cpp
@@ -2,13 +2,41 @@
 
 #include <QTimer>
 
+#include "presenters/i_load_simulation_presenter.h"
+#include "presenters/i_new_simulation_presenter.h"
 #include "views/main_window/i_main_window_view.h"
 
-MainWindowPresenter::MainWindowPresenter(IMainWindowView* view, QObject* parent)
-    : QObject(parent), view_(view), home_screen_timer_(new QTimer(this)) {
+namespace {
+constexpr int kHomeScreenRefreshRateFps = 30;
+constexpr int kNewSimulationTabIndex = 1;
+constexpr int kLoadSimulationTabIndex = 2;
+}  // namespace
+
+MainWindowPresenter::MainWindowPresenter(IMainWindowView* view,
+                                         ILoadSimulationPresenter* load_simulation_presenter,
+                                         INewSimulationPresenter* new_simulation_presenter,
+                                         QObject* parent)
+    : QObject(parent),
+      view_(view),
+      load_simulation_presenter_(load_simulation_presenter),
+      new_simulation_presenter_(new_simulation_presenter),
+      home_screen_timer_(new QTimer(this)) {
     Q_ASSERT(view_ != nullptr);
     // Update the home screen particle preview 30 times per second
     connect(home_screen_timer_, &QTimer::timeout, this, &MainWindowPresenter::updateHomeScreen);
-    const int fps = 30;  // Frames per second
-    home_screen_timer_->start(1000 / fps);
+    home_screen_timer_->start(1000 / kHomeScreenRefreshRateFps);
+}
+
+void MainWindowPresenter::onTabSwitched() {
+    auto index = view_->getCurrentTabIndex();
+    if (index == kLoadSimulationTabIndex) {
+        load_simulation_presenter_->active();
+        new_simulation_presenter_->inactive();
+    } else if (index == kNewSimulationTabIndex) {
+        load_simulation_presenter_->inactive();
+        new_simulation_presenter_->active();
+    } else {
+        load_simulation_presenter_->inactive();
+        new_simulation_presenter_->inactive();
+    }
 }
--- app/src/presenters/main_window_presenter.h
+++ app/src/presenters/main_window_presenter.h
@@ -5,6 +5,8 @@
 #include "views/main_window/i_main_window_view.h"
 
 class QTimer;
+class ILoadSimulationPresenter;
+class INewSimulationPresenter;
 
 /**
  * @brief Responsible for managing the main window's view and updating the home screen.
@@ -16,10 +18,21 @@ public:
      * @brief Initializes the presenter with the given view and sets up a timer to update the home
      * screen.
      * @param view The main window view to be managed.
+     * @param load_simulation_presenter The presenter for the load simulation tab.
+     * @param new_simulation_presenter The presenter for the new simulation tab.
      * @param parent The parent QObject, defaults to nullptr.
      */
-    explicit MainWindowPresenter(IMainWindowView* view, QObject* parent = nullptr);
+    explicit MainWindowPresenter(IMainWindowView* view,
+                                 ILoadSimulationPresenter* load_simulation_presenter,
+                                 INewSimulationPresenter* new_simulation_presenter,
+                                 QObject* parent = nullptr);
     ~MainWindowPresenter() override = default;
+
+public slots:
+    /**
+     * Activates the current tab and deactivates all others.
+     */
+    void onTabSwitched();
 
 private slots:
     void updateHomeScreen() { view_->updateHomeScreen(); }
@@ -27,4 +40,7 @@ private slots:
 private:
     IMainWindowView* view_ = nullptr;
     QTimer* home_screen_timer_ = nullptr;
+
+    ILoadSimulationPresenter* load_simulation_presenter_ = nullptr;
+    INewSimulationPresenter* new_simulation_presenter_ = nullptr;
 };
